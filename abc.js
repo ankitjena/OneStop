bfa6443634a5a5f9383717ef58db0b628122eaaf
ganeshpatro321 <ganeshpatro321@gmail.com>
2018-11-03 17:21:00 +0530

    facebook authentication

---
+++ abc.js
@@ -0,0 +1,9 @@
+// <GoogleLogin
+//     clientId={config.GOOGLE_CLIENT_ID}
+//     buttonText="Login"
+//     onSuccess={this.googleResponse}
+//     onFailure={this.onFailure}
+// />
+// <TwitterLogin loginUrl="http://localhost:4000/api/v1/auth/twitter"
+//                onFailure={this.onFailure} onSuccess={this.twitterResponse}
+//                requestTokenUrl="http://localhost:4000/api/v1/auth/twitter/reverse"/>
